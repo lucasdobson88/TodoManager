99a18260836647310ddf4123fe86c4dae602419f
Lucas Dobson <lucas.dobson@gecurrent.com>
2021-04-06 11:11:52 +1000

    added extra unit tests

--- src/app/core/services/tasks/tasks.service.spec.ts
+++ src/app/core/services/tasks/tasks.service.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed } from '@angular/core/testing';
+import { Task } from '@app/shared';
 
 import { TasksService } from './tasks.service';
 
-describe('TasksService', () => {
+describe("TasksService", () => {
   let service: TasksService;
 
   beforeEach(() => {
@@ -10,7 +11,22 @@ describe('TasksService', () => {
     service = TestBed.inject(TasksService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  fit("should add a value to the tasks when I create", done => {
+    const task = new Task({
+      title: "A test task",
+      description: "This is a test task",
+      priority: 0,
+      type: "Personal"
+    });
+    service.create(task);
+
+    service.tasks$.subscribe(tasks => {
+      expect(tasks.find(t => t.title === task.title).id).toBeTruthy();
+      done();
+    });
   });
 });
